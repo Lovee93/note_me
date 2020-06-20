802c9d335e8013ae12a91dd8b9bf6910215048f7
Lovee93 <lovee.jain93@gmail.com>
2020-06-20 19:42:21 +1000

    Improving the edit functionality by adding textarea and removing prompts

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Note.css';
 import { Textfit } from 'react-textfit';
 import edit from './edit.svg';
+import save from './save.svg';
 import del from './delete.png';
 
-function Note(props) {
+class Note extends Component {
 
-const {note, loadNotes} = props;
+	constructor(props) {
+		super(props);
+		this.state = {
+			edit_flag: false,
+			note_val: ''
+		}
+	}
 
-const delNote = () => {
+	delNote = () => {
+    const {note, loadNotes} = this.props;
 		fetch(`https://still-bastion-77945.herokuapp.com/del_note/${note.user_id}`, {
       method: 'delete',
       headers: {
@@ -27,39 +35,69 @@ const delNote = () => {
     })
 	}
 
-const	editNote = () => {
-		const prompt_note = prompt(`Current: ${note.note}\nNew: `)
-		if(prompt_note === '' || prompt_note === null) {
-			return;
-		} else {
-			fetch(`https://still-bastion-77945.herokuapp.com/edit_note/${note.user_id}`, {
+	saveChanges = () => {
+		const {note, loadNotes} = this.props;
+
+		fetch(`https://still-bastion-77945.herokuapp.com/edit_note/${note.user_id}`, {
 		      method: 'PUT',
 		      headers: {'Content-Type': 'application/json'},
 		      body: JSON.stringify({
 		        note_id: note.note_id,
-		        note_rev: prompt_note
+		        note_rev: this.state.note_val
 		      }) 
 		    })
 		    .then(response => response.json())
 		    .then(data => {
 		    	if(data) {
 		    		loadNotes(note.user_id)
+		    		this.setState({
+		    			edit_flag: false, 
+		    			note_val: ''
+		    		})
 		    	}
-		  })
-		}
+		  	})
 	}
+
+	handleTextAreaChange = (event) => {
+		this.setState({note_val: event.target.value})
+	}
+
+	editNote = () => {
+		this.setState({
+			edit_flag: true,
+			note_val: this.props.note.note
+		})
+	}
+
+	render() {
+		const { note, loadNotes } = this.props;
+		const { edit_flag, note_val } = this.state;
 
 		return(	
 			<div className="note_div">
-		    <Textfit mode="multi" className="note">
-		        {note.note}
-		    </Textfit>
-		    <div className="options">
-		    	<button className="icon" onClick={editNote} ><img src={edit} alt='edit_note' /></button>
-		    	<button className="icon" onClick={delNote} ><img src={del} alt='del_note' /></button>
-		    </div>
+			  {
+			  	!edit_flag ? 
+			  	<>  
+			    <Textfit mode="multi" className="note">
+			        {note.note}
+			    </Textfit>
+			    <div className="options">
+			    	<button className="icon" onClick={this.editNote} ><img src={edit} alt='edit_note' /></button>
+			    	<button className="icon" onClick={this.delNote} ><img src={del} alt='del_note' /></button>
+			    </div>
+			   </>
+			   :
+			   <>  
+			    <textarea className="note f3" value={note_val} onChange={this.handleTextAreaChange} />
+			    <div className="options">
+			    	<button className="icon" onClick={this.saveChanges} ><img src={save} alt='edit_note' /></button>
+			    	<button className="icon" onClick={this.delNote} ><img src={del} alt='del_note' /></button>
+			    </div>
+			   </>
+			 	}
     	</div>
 		);
+	}
 }
 
 export default Note;
